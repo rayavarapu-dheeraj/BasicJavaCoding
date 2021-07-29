b2c036b017495475cd437d76fe171dab678bf4e2
rayavarapu-dheeraj <rayavarapu059@gmail.com>
2021-07-29 21:05:37 +0530

    with ignore

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,9 +1,12 @@
 
 public class HelloWorld {
 	public static void main(String [] args) {
+		int a = 10;
+		if(a>=10) {
 		for(int i=0;i<10;i++) {
 		System.out.println("Hello world");
 		System.out.println("Into git");
+	}
 	}
 	}
 
